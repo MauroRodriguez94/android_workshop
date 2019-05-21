55f856980e1aef8f82a23ad81415138e2ca84962
lugarbarini <lucia.garbarini@mercadolibre.com>
2019-05-21 09:58:08 -0300

    part 6 - ids

--- app/src/main/java/com/example/androidworkshop/LoginActivity.java
+++ app/src/main/java/com/example/androidworkshop/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.androidworkshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText userNameInput = findViewById(R.id.user_name);
+        EditText passwordInput = findViewById(R.id.user_password);
+        Button continueButton = findViewById(R.id.continue_button);
     }
 }
